1680e3ba08e576c5c3113f9420027cdef8b970e0
Sachin Sharma <sachin2252sharma@gmail.com>
2022-11-09 00:03:37 +0530

    added responsiveness in profile card

--- src/quiz/components/profile/index.tsx
+++ src/quiz/components/profile/index.tsx
@@ -6,8 +6,8 @@ const ProfileCard = ({userData}:any) => {
             <div className='profile-wrapper container-fluid w-50 h-50 card rounded-3 container-lg'>
                 <h2 className='text-center text-light mt-3 border-bottom'>{userData.username}'s Profile Details</h2>
                 <div
-                    className='profile-details mt-4 mx-4 w-75 d-flex flex-wrap justify-content-center align-items-center'>
-                    <div className='w-50 h-100 text-light text-start'>
+                    className='profile-details mt-4 mx-4 w-100 d-flex flex-wrap justify-content-center align-items-center'>
+                    <div className='w-100 h-100 text-light text-start'>
                         <h6 className='mx-2 my-3 text-start'>QUIZ CREATED : {userData?.totalQuizes}</h6>
                         <h6 className='mx-2 my-3 text-start'>NAME : {userData.username}</h6>
                         <h6 className='mx-2 my-3 text-start'>USERNAME : {userData.username}</h6>
